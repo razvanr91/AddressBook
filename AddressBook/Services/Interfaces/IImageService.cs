2956c42c56be8511a55a5f7da2dd879de721aa51
razvanr91 <razvanroman1991@gmail.com>
2021-07-25 21:44:12 +0100

    Add Image Service

---
+++ AddressBook/Services/Interfaces/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AddressBook.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
