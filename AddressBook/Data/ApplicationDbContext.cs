219d08ff0adabd91ef356840642c7ddaa790fb80
razvanr91 <razvanroman1991@gmail.com>
2021-07-25 04:43:35 +0100

    Create App Scheleton

---
+++ AddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
